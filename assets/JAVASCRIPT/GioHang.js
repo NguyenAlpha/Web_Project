b8058722d3a232d8e6d49ea3d069595221b95343
TheKien chim to <nguyenkienvt9@gmail.com>
2024-10-22 13:50:49 +0700

    kiên 2

--- assets/JAVASCRIPT/GioHang.js
+++ assets/JAVASCRIPT/GioHang.js
@@ -1,1 +1,11 @@
 // add sản phẩm vào giỏ hàng
+const btn = document.querySelectorAll("button")
+// console.log(btn)
+btn.addEventListenrt("click",function(envent)){
+    {
+        var btnItem = envent.target
+        var product = btnItem.parentElement
+        console.log(product)
+    }
+}
+// ==================================Tổng tiền================================
