d7bde81846251d1f9eb6d478ac40ca023023ac4a
TheKien chim to <nguyenkienvt9@gmail.com>
2024-10-31 00:49:22 +0700

    kiên 31/10

--- assets/JAVASCRIPT/GioHang.js
+++ assets/JAVASCRIPT/GioHang.js
@@ -1,14 +1,23 @@
 // add sản phẩm vào giỏ hàng
-const btn = document.querySelectorAll("i.fa-solid.fa-cart-shopping.product__cart")
+const btn = document.querySelectorAll(
+  "i.fa-solid.fa-cart-shopping.product__cart"
+);
 // console.log(btn)
-btn.forEach(function(element,index) //Hiện phần tử và chỉ số cho nó 
-{
-   element.addEventListener("click",function(event)
-{{
-    var btnItem = event.target
-    var product = btnItem.parentElement
-    console.log(product)
-}})
-})
+btn.forEach(function (
+  element,
+  index //Hiện phần tử và chỉ số cho nó
+) {
+  element.addEventListener("click", function (event) {
+    {
+      var btnItem = event.target;
+      var product = btnItem.parentElement; // Hiện thị thẻ con cấp 1
+      var product2 = product.parentElement; //Hiện thị thẻ con cấp 2
+      var product3 = product2.parentElement; //Hiện thị thẻ con cấp 3
+      //   var productImg = product.querySelector("img");
+      //   console.log(img);
+      console.log(product3);
+    }
+  });
+});
 
 // ==================================Tổng tiền================================
