aa5ff099951e6cbef4bbbbb682605669f2e76d3b
TheKien chim to <nguyenkienvt9@gmail.com>
2024-10-30 16:44:14 +0700

    kiên bumb 30/10

--- assets/JAVASCRIPT/GioHang.js
+++ assets/JAVASCRIPT/GioHang.js
@@ -1,11 +1,14 @@
 // add sản phẩm vào giỏ hàng
-const btn = document.querySelectorAll("button")
+const btn = document.querySelectorAll("i.fa-solid.fa-cart-shopping.product__cart")
 // console.log(btn)
-btn.addEventListenrt("click",function(envent)){
-    {
-        var btnItem = envent.target
-        var product = btnItem.parentElement
-        console.log(product)
-    }
-}
+btn.forEach(function(element,index) //Hiện phần tử và chỉ số cho nó 
+{
+   element.addEventListener("click",function(event)
+{{
+    var btnItem = event.target
+    var product = btnItem.parentElement
+    console.log(product)
+}})
+})
+
 // ==================================Tổng tiền================================
