5b3b42a3e54afa0767690fbc1ecdba12aa17979a
TheKien chim to <nguyenkienvt9@gmail.com>
2024-11-01 10:40:00 +0700

    kiên 1/11

--- assets/JAVASCRIPT/GioHang.js
+++ assets/JAVASCRIPT/GioHang.js
@@ -2,22 +2,23 @@
 const btn = document.querySelectorAll(
   "i.fa-solid.fa-cart-shopping.product__cart"
 );
-// console.log(btn)
-btn.forEach(function (
-  element,
-  index //Hiện phần tử và chỉ số cho nó
-) {
+
+btn.forEach(function (element, index) {
   element.addEventListener("click", function (event) {
-    {
-      var btnItem = event.target;
-      var product = btnItem.parentElement; // Hiện thị thẻ con cấp 1
-      var product2 = product.parentElement; //Hiện thị thẻ con cấp 2
-      var product3 = product2.parentElement; //Hiện thị thẻ con cấp 3
-      //   var productImg = product.querySelector("img");
-      //   console.log(img);
-      console.log(product3);
-    }
+    var btnItem = event.target;
+    var product = btnItem.parentElement; // Hiển thị thẻ con cấp 1
+    var product2 = product.parentElement; // Hiển thị thẻ con cấp 2
+    var product3 = product2.parentElement; // Hiển thị thẻ con cấp 3
+    var product4 = product3.parentElement; // Hiển thị thẻ con cấp 4
+    var productImg = product3.querySelector("img").src; // Đọc hình
+    var productName = product3.querySelector("a.product__name").innerText; // tên sản phẩm
+    var productPrice = product3.querySelector("span.product__new").innerText; // giá sản phẩm
+    console.log(productPrice); // Corrected line
+    console.log(productImg);
+    console.log(productName);
+    addcart(productImg, productName, productPrice);
   });
 });
 
-// ==================================Tổng tiền================================
+
+// ==================================Tổng tiền===========================
